f90db5b57ec0a3d68a2aeab682541b06af9d58b5
amagiddmxh <amagiddmxh@gmail.com>
2021-12-24 01:33:20 +0800

    Add new zh translation, update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import {
   Box,
   LinkBox,
   LinkOverlay,
+  Button,
+  Spacer,
 } from "@chakra-ui/react"
 import { GetStaticProps } from "next"
 import { useTranslation } from "next-i18next"
@@ -34,14 +36,94 @@ export default function IndexPage() {
         >
           <LinkBox h="60px">
             <LinkOverlay href={websiteLink}>
-              <Heading as="h1" fontSize="2xl">
-                {t("title")}
-              </Heading>
+              <Heading as="h1">{t("title")}</Heading>
             </LinkOverlay>
           </LinkBox>
-          <Text fontSize="xs" verticalAlign="bottom">{t("description")}</Text>
+          <Text fontSize="xs" verticalAlign="bottom">
+            {t("description")}
+          </Text>
         </HStack>
-        <Stack spacing={{ base: 10, md: 12 }}></Stack>
+        <Stack spacing="4">
+          <Heading as="h4" fontSize="sm">
+            {t("mission")}
+          </Heading>
+          <Button width="md">{t("action.gotoapp")}</Button>
+        </Stack>
+
+        {/* Problems */}
+        <Stack spacing="4">
+          <Heading as="h2" fontSize="lg">
+            {t("problems.title")}
+          </Heading>
+          <Heading as="h2" fontSize="lg">
+            {t("problems.edu.title")}
+          </Heading>
+          <Stack spacing="6">
+            <Stack>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.admittance.title")}
+              </Text>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.admittance.description")}
+              </Text>
+            </Stack>
+            <Stack>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.system.title")}
+              </Text>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.system.description")}
+              </Text>
+            </Stack>
+            <Stack>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.evaluation.title")}
+              </Text>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.evaluation.description")}
+              </Text>
+            </Stack>
+            <Stack>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.autonomy.title")}
+              </Text>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.autonomy.description")}
+              </Text>
+            </Stack>
+            <Stack>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.reward.title")}
+              </Text>
+              <Text as="p" fontSize="sm">
+                {t("problems.edu.reward.description")}
+              </Text>
+            </Stack>
+          </Stack>
+
+          {/* Changes */}
+          <Heading as="h2" fontSize="lg">
+            {t("changes.title")}
+          </Heading>
+          <Stack spacing="6">
+            <Stack>
+              {/* Use HTML to wrap new line */}
+              <Text
+                as="p"
+                fontSize="sm"
+                whiteSpace="pre-wrap"
+                dangerouslySetInnerHTML={{ __html: t("changes.edu") }}
+              />
+              <Text
+                as="p"
+                fontSize="sm"
+                whiteSpace="pre-wrap"
+                dangerouslySetInnerHTML={{ __html: t("changes.research") }}
+              />
+            </Stack>
+          </Stack>
+        </Stack>
+        {/* TODO: Rest of home content */}
       </Stack>
     </Container>
   )
