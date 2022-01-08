20983631f1330aee7d0928c62363a7e6bb15df9e
bbcfive <bbcfive@163.com>
2022-01-08 14:25:12 +0800

    fix pipeline

--- pages/guildInfo.tsx
+++ pages/guildInfo.tsx
@@ -6,17 +6,16 @@ import { Flex } from '@components/Flex';
 
 export default function GuildInfo({ guild }: { guild: GuildListType }) {
   const { t } = useTranslation();
-  let { nfts, guilds } = guild.members;
 
   return (
     <>
-      <p key={guild.name}>{guild.name}</p>
-      <p key={guild.guild_id}>{nfts.length + guilds.length + " members"}</p>
+      <p key={guild?.name}>{guild?.name}</p>
+      <p key={guild?.guild_id}>{guild?.members.nfts.length + guild?.members.guilds.length + " members"}</p>
       <Flex>
-        {nfts?.map((nft) => (
+        {guild?.members.nfts?.map((nft) => (
           <p key={nft.name}>{nft.name}</p>
         ))}
-        {guilds?.map((memberGuild) => (
+        {guild?.members.guilds?.map((memberGuild) => (
           <p key={memberGuild}>{memberGuild}</p>
         ))}
       </Flex>
