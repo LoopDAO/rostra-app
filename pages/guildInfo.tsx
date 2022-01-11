eb0d73ea0a015f9d3bcd7cf3bc7fd2a12bc989dd
bbcfive <bbcfive@163.com>
2022-01-11 23:47:37 +0800

    add guildDetails

--- pages/guildInfo.tsx
+++ pages/guildInfo.tsx
@@ -6,25 +6,45 @@ import { Fieldset } from '@components/common/Fieldset'
 import { GetStaticProps } from "next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { Text } from '@components/common/Text'
+import { useRouter } from "next/router"
 
 export default function GuildInfo({ guild }: { guild: GuildListType }) {
   const { t } = useTranslation()
+  const nfts = guild?.members?.nfts;
+  const guilds = guild?.members?.guilds;
+  const router = useRouter()
+  const handleClick = (guild: GuildListType) => {
+    router.push({
+      pathname: '/guildDetails',
+      query: { guild: JSON.stringify(guild) }
+    })
+  }
 
   return (
-    <>
-      <p key={guild?.name}>{guild?.name}</p>
-      <Text key={guild?.guild_id}>
-        {guild?.members.nfts.length + guild?.members.guilds.length + " members"}
+    <div onClick={() => handleClick(guild)}>
+      <Text key={guild?.name}>{guild?.name}</Text>
+      <Text css={{ color: "Gray", fontSize: "12px", marginTop: "5px" }} key={guild?.guild_id}>
+        {nfts?.length + guilds?.length + " members"}
       </Text>
-        <Fieldset css={{ gap: 10  }}>
-          {guild?.members.nfts?.map((nft) => (
-            <p key={nft.name}>{nft.name}</p>
+        <Fieldset css={{ marginTop: "5px" }}>
+          {nfts?.map((nft) => (
+            <Text
+              css={{ backgroundColor: "Gray",
+              color: "white",
+              border: "3px solid gray",
+              borderRadius: "5px"
+            }} key={nft.name}>{nft.name}</Text>
           ))}
-          {guild?.members.guilds?.map((memberGuild) => (
-            <p key={memberGuild}>{memberGuild}</p>
+          {guilds?.map((memberGuild) => (
+            <Text css={{
+              backgroundColor: "#7be2f9",
+              color: "white",
+              border: "3px solid #7be2f9",
+              borderRadius: "5px"
+            }} key={memberGuild}>{memberGuild}</Text>
           ))}
         </Fieldset>
-    </>
+    </div>
   )
 }
 
