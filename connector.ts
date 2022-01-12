18db6a2be32b4ff62c6b8f8533ebc6b261353c29
AmagiDDmxh <amagiddmxh@gmail.com>
2022-01-13 01:35:42 +0800

    Polish connector

--- connector.ts
+++ connector.ts
@@ -1,19 +1,13 @@
 import { InjectedConnector } from "@web3-react/injected-connector"
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
+// Add it later
+// import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
 
-const chains: { [key: string]: number } = {
-  ETHEREUM: 1,
-  BSC: 56,
-  POLYGON: 137,
-  AVALANCHE: 43114,
-  XDAI: 100,
-  FANTOM: 250,
-  ARBITRUM: 42161,
-  CELO: 42220,
-  HARMONY: 1666600000,
+export enum Chains {
+  ETHEREUM = 1,
+  POLYGON = 137,
 }
 
-const RPC: { [key: string]: any } = {
+export const RPC: { [key: string]: any } = {
   ETHEREUM: {
     chainId: 1,
     chainName: "Ethereum",
@@ -26,23 +20,8 @@ const RPC: { [key: string]: any } = {
         "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
     },
     blockExplorerUrls: ["https://etherscan.io"],
-    iconUrls: ["/networkLogos/ethereum.svg"],
+    iconUrls: ["/image/network/ethereum.svg"],
     rpcUrls: ["https://main-light.eth.linkpool.io"],
-  },
-  BSC: {
-    chainId: "0x38",
-    chainName: "BSC",
-    nativeCurrency: {
-      name: "Binance Coin",
-      symbol: "BNB",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/825/small/binance-coin-logo.png?1547034615",
-    },
-    rpcUrls: ["https://bsc-dataseed.binance.org"],
-    blockExplorerUrls: ["https://bscscan.com"],
-    iconUrls: ["/networkLogos/bsc.svg"],
   },
   POLYGON: {
     chainId: "0x89",
@@ -57,125 +36,24 @@ const RPC: { [key: string]: any } = {
     },
     rpcUrls: ["https://polygon-rpc.com"],
     blockExplorerUrls: ["https://polygonscan.com"],
-    iconUrls: ["/networkLogos/polygon.svg"],
+    iconUrls: ["/image/network/polygon.svg"],
   },
-  AVALANCHE: {
-    chainId: 43114,
-    chainName: "Avalanche Mainnet",
-    nativeCurrency: {
-      name: "Avalanche",
-      symbol: "AVAX",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/12559/small/coin-round-red.png?1604021818",
-    },
-    rpcUrls: ["https://api.avax.network/ext/bc/C/rpc"],
-    blockExplorerUrls: ["https://snowtrace.io"],
-    iconUrls: ["/networkLogos/avalanche.svg"],
-  },
-  XDAI: {
-    chainId: 100,
-    chainName: "xDAI Chain",
-    nativeCurrency: {
-      name: "xDAI",
-      symbol: "XDAI",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/11062/small/xdai.png?1614727492",
-    },
-    rpcUrls: ["https://rpc.xdaichain.com"],
-    blockExplorerUrls: ["https://blockscout.com/poa/xdai"],
-    iconUrls: ["/networkLogos/xdai.svg"],
-  },
-  FANTOM: {
-    chainId: 250,
-    chainName: "Fantom Opera",
-    nativeCurrency: {
-      name: "Fantom",
-      symbol: "FTM",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/4001/small/Fantom.png?1558015016",
-    },
-    rpcUrls: ["https://rpc.ftm.tools"],
-    blockExplorerUrls: ["https://ftmscan.com"],
-    iconUrls: ["/networkLogos/fantom.svg"],
-  },
-  ARBITRUM: {
-    chainId: 42161,
-    chainName: "Arbitrum One",
-    nativeCurrency: {
-      name: "Ether",
-      symbol: "AETH",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
-    },
-    rpcUrls: ["https://arb1.arbitrum.io/rpc"],
-    blockExplorerUrls: ["https://arbiscan.io"],
-    iconUrls: ["/networkLogos/arbitrum.svg"],
-  },
-  CELO: {
-    chainId: 42220,
-    chainName: "Celo Mainnet",
-    nativeCurrency: {
-      name: "Celo",
-      symbol: "CELO",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/11090/small/icon-celo-CELO-color-500.png?1592293590",
-    },
-    rpcUrls: ["https://forno.celo.org"],
-    blockExplorerUrls: ["https://explorer.celo.org"],
-    iconUrls: ["/networkLogos/celo.svg"],
-  },
-  HARMONY: {
-    chainId: 1666600000,
-    chainName: "Harmony",
-    nativeCurrency: {
-      name: "Harmony",
-      symbol: "ONE",
-      decimals: 18,
-      address: "0x0000000000000000000000000000000000000000",
-      logoURI:
-        "https://assets.coingecko.com/coins/images/4344/small/Y88JAze.png?1565065793",
-    },
-    rpcUrls: ["https://api.harmony.one"],
-    blockExplorerUrls: ["https://explorer.harmony.one"],
-    iconUrls: ["/networkLogos/harmony.svg"],
-  },
-}
+} as const
 
-const supportedChains = [
-  "ETHEREUM",
-  "POLYGON",
-  "AVALANCHE",
-  "XDAI",
-  "FANTOM",
-  "ARBITRUM",
-  "CELO",
-  "HARMONY",
-  "BSC",
-]
-const supportedChainIds = supportedChains.map((_: string) => chains[_])
+export const supportedChains = ["ETHEREUM"]
+export const supportedChainIds = supportedChains.map((_: string) => Chains[_])
 
-const injected = new InjectedConnector({ supportedChainIds })
+export const injected = new InjectedConnector({ supportedChainIds })
 
-const walletConnect = new WalletConnectConnector({
-  supportedChainIds,
-  rpc: Object.keys(RPC).reduce(
-    (obj, chainName) => ({
-      ...obj,
-      [chains[chainName]]: RPC[chainName].rpcUrls[0],
-    }),
-    {}
-  ),
-  qrcode: true,
-})
-
-export { chains, RPC, supportedChains, injected, walletConnect }
+// TODO: Add walletconnect
+// export const walletConnect = new WalletConnectConnector({
+//   supportedChainIds,
+//   rpc: Object.keys(RPC).reduce(
+//     (obj, chainName) => ({
+//       ...obj,
+//       [Chains[chainName as keyof typeof Chains]]: RPC[chainName].rpcUrls[0],
+//     }),
+//     {}
+//   ),
+//   qrcode: true,
+// })
