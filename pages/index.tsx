965f1a0b81e4c1ff0646eb40b4a13d1934fd2742
HA <>
2022-02-11 15:09:01 +0800

    fix(home): remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import { GetStaticProps } from "next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import Hero from './Hero'
-import { Box, HStack, Center, Link, VStack } from "@chakra-ui/react";
 
 export default function IndexPage() {
-  const contentHigh = document.documentElement.clientHeight - 64 * 2  // header + footer
-
   return (
-    <Box h={`${contentHigh}px`}>
+    <>
       <Hero />
-    </Box>
+    </>
   )
 }
 
