41aa7a2e2f7eb586a753c85c1f49308fe08ac857
AmagiDDmxh <amagiddmxh@gmail.com>
2022-02-22 14:23:48 +0800

    fix: header wrong rebased codes

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,30 +1,21 @@
-import { ReactNode } from 'react'
-import {
-  Box,
-  Flex,
-  HStack,
-  Button,
-  useColorModeValue,
-} from '@chakra-ui/react'
-import { AddIcon } from '@chakra-ui/icons'
+import { ReactNode } from "react"
+import { Box, Flex, HStack, Button, useColorModeValue } from "@chakra-ui/react"
+import { AddIcon } from "@chakra-ui/icons"
 import { useTranslation } from "next-i18next"
-import Link from 'next/link'
+import Link from "next/link"
 import LocaleToggle from "./LocaleToggle"
 import Account from "./Account"
-import { useAccountFlashsigner } from '@lib/hooks/useAccount'
-import AccountFlashsigner from './Account/AccountFlashsigner'
+import { useAccountFlashsigner } from "@lib/hooks/useAccount"
+import AccountFlashsigner from "./Account/AccountFlashsigner"
 
-const Links = ['guild.guilds'] as const
+const Links = ["guild.guilds"] as const
 const hrefs = {
-  'guild.guilds': '/guild',
-  'guild.create': '/guild/create',
+  "guild.guilds": "/guild",
+  "guild.create": "/guild/create",
 } as const
 
-const NavLink = ({ children, href }: { children: ReactNode, href: any }) => (
-  <Link
-    href={href}>
-    {children}
-  </Link>
+const NavLink = ({ children, href }: { children: ReactNode; href: any }) => (
+  <Link href={href}>{children}</Link>
 )
 
 export default function Header() {
@@ -33,30 +24,32 @@ export default function Header() {
 
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <HStack spacing={8} alignItems={'center'}>
-            <Link href='/' passHref>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <HStack spacing={8} alignItems={"center"}>
+            <Link href="/" passHref>
               Rostra
             </Link>
-            <HStack
-              as={'nav'}
-              spacing={4}
-            >
-              <NavLink key={'/guild'} href='/guild'>{t('/guild')}</NavLink>
+            <HStack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link} href={hrefs[link]}>
+                  {t(link)}
+                </NavLink>
+              ))}
             </HStack>
           </HStack>
 
-          <Flex alignItems={'center'}>
+          <Flex alignItems={"center"}>
             {/* <LocaleToggle /> */}
-            <Link href='/guild/create' passHref>
+            <Link href="/guild/create" passHref>
               <Button
-                variant={'solid'}
-                colorScheme={'green'}
-                size={'sm'}
+                variant={"solid"}
+                colorScheme={"green"}
+                size={"sm"}
                 ml={4}
                 mr={4}
-                leftIcon={<AddIcon />}>
+                leftIcon={<AddIcon />}
+              >
                 Guild
               </Button>
             </Link>
