0911d5d5475e9f1f9f20458f56ba5aea88f8af0b
bbcfive <bbcfive@163.com>
2022-03-06 21:36:28 +0800

    fix: fix comments

--- components/IPFS/saveToIpfs.ts
+++ components/IPFS/saveToIpfs.ts
@@ -4,6 +4,5 @@ export const saveToIpfs = async (data: any) => {
   const ipfs = await IPFS.create()
   const { cid } = await ipfs.add(JSON.stringify(data))
 
-  console.log("IPFS address", cid.toString());
   return cid.toString();
 }
