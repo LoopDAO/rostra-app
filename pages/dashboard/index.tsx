5c4dfe412f32fc7f8295491cfdb181102b00a513
HA <>
2022-03-24 15:20:12 +0800

    feat: register cota with flashsigner

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { useTranslation } from "next-i18next"
 import { GetStaticProps } from "next"
+import { useRouter } from 'next/router'
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import { Flex, Heading, Stack, Container } from "@chakra-ui/react"
+import { Flex, Heading, Stack, Container, Button } from "@chakra-ui/react"
 import { useAccountFlashsigner } from "@lib/hooks/useAccount"
 import AccountFlashsigner from "../../components/Layout/Account/AccountFlashsigner"
 import {
@@ -10,7 +11,8 @@ import {
   serializeScript,
   scriptToHash,
   rawTransactionToHash,
-  serializeWitnessArgs
+  serializeWitnessArgs,
+  serializeWitnesses
 } from '@nervosnetwork/ckb-sdk-utils'
 import {
   Service,
@@ -30,6 +32,12 @@ import {
   MintCotaInfo,
   TransferWithdrawal,
 } from '@nervina-labs/cota-sdk'
+import signWitnesses from '@nervosnetwork/ckb-sdk-core/lib/signWitnesses'
+import { signMessageWithRedirect, signTransactionWithRedirect, appendSignatureToTransaction, Config } from '@nervina-labs/flashsigner'
+import paramsFormatter from '@nervosnetwork/ckb-sdk-rpc/lib/paramsFormatter'
+import { getResultFromURL, FlashsignerAction } from '@nervina-labs/flashsigner'
+
+Config.setChainType('testnet')
 
 const service: Service = {
   collector: new Collector({
@@ -37,12 +45,60 @@ const service: Service = {
   }),
   aggregator: new Aggregator({ registryUrl: 'http://cota-registry-aggregator.rostra.xyz', cotaUrl: 'http://cota-aggregator.rostra.xyz' }),
 }
+const ckb = service.collector.getCkb()
+
+const registerCota = async () => {
+  const address = 'ckt1qpth5hjexr3wehtzqpm97dzzucgemjv7sl05wnez7y72hqvuszeyyqt90590gs808qzwq8uj2z6hhr4wrs70vrgmamexx'
+  const provideCKBLock = addressToScript(address)
+  const unregisteredCotaLock = addressToScript(address)
+  let rawTx = await generateRegisterCotaTx(service, [unregisteredCotaLock], provideCKBLock)
+  // const flashsingerDep = Config.getCellDep()
+  const flashsingerDep: CKBComponents.CellDep = {
+    outPoint: {
+      txHash: '0xb66776ff3244033fcd15312ae8b17d384c11bebbb923fce3bd896d89f4744d48',
+      index: '0x0',
+    },
+    depType: 'depGroup',
+  }
+  console.log('celldep: ', flashsingerDep)
+  rawTx.cellDeps.push(flashsingerDep)
+
+  const registryLock = getAlwaysSuccessLock(false)
+
+  const cells = rawTx.inputs.map((input, index) => ({
+    outPoint: input.previousOutput,
+    lock: index === 0 ? registryLock : provideCKBLock,
+  }))
+
+  const transactionHash = rawTransactionToHash(rawTx)
+
+  const witnesses = {
+    transactionHash,
+    witnesses: rawTx.witnesses,
+    inputCells: cells,
+    skipMissingKeys: true,
+  }
+
+  console.log('witnesses', JSON.stringify(witnesses, null, 2))
+
+  const tx: any = paramsFormatter.toRawTransaction({
+    ...rawTx,
+    witnesses: witnesses.witnesses.map((witness) =>
+      typeof witness === 'string' ? witness : serializeWitnessArgs(witness)
+    )
+  })
+  signTransactionWithRedirect(
+    'http://localhost:3000/dashboard?sig=',
+    { tx }
+  )
+}
 
 export default function DashboardPage() {
   const { t } = useTranslation()
   const { account, isLoggedIn } = useAccountFlashsigner()
 
   const [status, setStatus] = useState(false);
+  const [signedTx, setSignedTx] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const res = await service.aggregator.checkReisteredLockHashes([
@@ -54,7 +110,51 @@ export default function DashboardPage() {
     fetchData();
   }, [account.address]);
 
+  const router = useRouter()
+  console.log('router.query: ', router);
+  if (router.query.action === 'sign-transaction') {
+    getResultFromURL(router.asPath, {
+      onLogin(res) {
+        const {
+          // 当登录成功时，flashsigner 会对 dapp 网站地址和时间戳进行签名，
+          // 并把要签名的信息和签名返回
+          message,
+          signature,
+          // 已授权账户的公钥
+          pubkey,
+          // 已授权账户的地址
+          address,
+          // 请求登录时的额外数据
+          extra,
+        } = res
+        console.log('onLogin res: ', res)
+
+      },
+      async onSignTransaction(res) {
+        const {
+
+          // 已签名的交易
+          transaction,
+          // 已签名账户的地址
+          address,
+          // 请求签名时的额外数据
+          extra,
+        } = res
+        console.log('onSignTransaction res.transaction: ', res.transaction)
+        console.log('onSignTransaction res.transaction: ', JSON.stringify(res.transaction))
+        const signedTx = ckb.rpc.resultFormatter.toTransaction(res.transaction as any)
+        let txHash = await ckb.rpc.sendTransaction(signedTx as any, 'passthrough')
+        console.log(`Register cota cell tx has been sent with tx hash ${txHash}`)
+      }
+    })
+  }
+
   if (!isLoggedIn) return <AccountFlashsigner />
+
+  let registryBtn
+  if(!status) {
+    registryBtn = <Button onClick={() => { registerCota() }}>Register</Button>
+  }
 
   return (
     <Stack spacing={2} p={4}>
@@ -63,7 +163,7 @@ export default function DashboardPage() {
       <Flex marginTop={4} flexWrap="wrap" gap={4} p={0}>
         <Container>Address: {account.address}</Container>
         <Container>Balance: {'todo'}</Container>
-        <Container>CKB CoTA Registry: {status.toString()}</Container>
+        <Container>CKB CoTA Registry: {status.toString()} {registryBtn}</Container>
       </Flex>
     </Stack>
   )
