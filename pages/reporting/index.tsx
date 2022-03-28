a30a63654afbe7b4e2473b005184ba26364edcb7
HA <>
2022-03-28 16:11:38 +0800

    feat: show reporting table with sample data

--- pages/reporting/index.tsx
+++ pages/reporting/index.tsx
@@ -1,39 +1,61 @@
+import React, { useState } from "react"
 import {
-    Tab,
-    TabList,
-    TabPanel,
-    TabPanels,
-    Tabs,
-} from "@chakra-ui/react"
-import RuleAction from "@components/setting/RuleAction"
-import RuleBaseInfo from "@components/setting/RuleBaseInfo"
-import RuleNFT from "@components/setting/RuleNFT"
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from '@chakra-ui/react'
 import { useAccountFlashsigner } from "@lib/hooks/useAccount"
-import { RuleType } from "api/rule_setting"
 import { GetStaticProps } from "next"
 import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import ErrorPage from "pages/ErrorPage"
-import SuccessPage from "pages/SuccessPage"
-import React, { useState } from "react"
 import Sidebar from "@components/Layout/Sidebar"
 
 export default function SettingPage() {
-    const { t } = useTranslation()
+  const { t } = useTranslation()
 
-    const { isLoggedIn: isLoggedInFlash, account: accountFlash } = useAccountFlashsigner()
+  const { isLoggedIn: isLoggedInFlash, account: accountFlash } = useAccountFlashsigner()
 
+  const addressList = [
+    'ckt1qyqfwyghxgvf3522cgutpaqruyy3gqugk3zqa8yddf',
+  'ckt1qpth5hjexr3wehtzqpm97dzzucgemjv7sl05wnez7y72hqvuszeyyqt90590gs808qzwq8uj2z6hhr4wrs70vrgmamexx',
+  'ckt1qqypm0l63rdt2jayymfrrjnyadmqe630a8skwcdpmfqqmgdje0sjsqt90590gs808qzwq8uj2z6hhr4wrs70vrg7wyejq',
+  ]
+
+  const trElems = addressList.map((address, index) => {
     return (
-      <Sidebar>
-        reporting
-      </Sidebar>
+      <Tr key={address}>
+        <Td>{address}</Td>
+        <Td>Delete</Td>
+      </Tr>
     )
+  })
+
+  return (
+    <Sidebar>
+      <Table size='sm'>
+        <Thead>
+          <Tr>
+            <Th>Address</Th>
+            <Th>Action</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {trElems}
+        </Tbody>
+      </Table>
+    </Sidebar>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale!, ["common"])),
-        },
-    }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, ["common"])),
+    },
+  }
 }
