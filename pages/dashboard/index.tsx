02f7b24b87ec10e059d4ede20208765e65ceb324
HA <>
2022-06-04 15:23:18 +0800

    show account info card on dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { useTranslation } from "next-i18next"
 import { GetStaticProps } from "next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import { Heading, Stack, Box } from "@chakra-ui/react"
 import { useAccountFlashsigner } from "@lib/hooks/useAccount"
 import AccountFlashsigner from "../../components/Layout/Account/AccountFlashsigner"
-import { addressToScript, scriptToHash } from '@nervosnetwork/ckb-sdk-utils'
+import { addressToScript } from '@nervosnetwork/ckb-sdk-utils'
 import { generateFlashsignerAddress } from "@nervina-labs/flashsigner"
-import { cotaService, ckbIndexerUrl, hexToBalance } from "@lib/utils/ckb"
+import { ckbIndexerUrl, hexToBalance } from "@lib/utils/ckb"
 import useSWR from "swr"
 import fetchers from "api/fetchers"
 import { QRCodeSVG } from "qrcode.react"
+import { Box, Center, Text, Stack } from "@chakra-ui/react"
+import CopyableAddress from "@components/Layout/Account/CopyableAddress"
 
 export default function DashboardPage() {
-  const { t } = useTranslation()
   const { account, isLoggedIn } = useAccountFlashsigner()
   const cotaAddress = generateFlashsignerAddress(account.auth.pubkey)
   const script = addressToScript(cotaAddress)
@@ -21,17 +21,46 @@ export default function DashboardPage() {
   const balance = hexToBalance(data?.result?.capacity)
 
   if (!isLoggedIn) return <AccountFlashsigner />
-
+  console.log("cotaAddress", cotaAddress)
   return (
     <Stack spacing={2} p={4}>
-      <Heading>{t("dashboard.title")}</Heading>
-      <Box>Address</Box>
-      <QRCodeSVG value={cotaAddress} />
-      <Box>{cotaAddress}</Box>
-      <Box>Balance: {balance} CKB</Box>
+      <Center py={12}>
+        <Box
+          role={"group"}
+          py={6}
+          maxW={"330px"}
+          w={"full"}
+          boxShadow={"2xl"}
+          rounded={"lg"}
+          pos={"relative"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          display={"flex"}
+        >
+          <Box
+            rounded={"lg"}
+            pos={"relative"}
+          >
+            <QRCodeSVG value={cotaAddress} />
+          </Box>
+          <Stack pt={10} align={"center"}>
+            <Text color={"gray.500"} fontSize={"sm"}>
+              <CopyableAddress address={cotaAddress} decimals={4} />
+            </Text>
+            <Stack direction={"row"} align={"center"}>
+              <Text>
+                {balance} CKB
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Center>
     </Stack>
   )
 }
+
+
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
