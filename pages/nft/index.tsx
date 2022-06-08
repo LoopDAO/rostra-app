3a34c802e9787510baa7f3d96ba39b0297466a86
HA <>
2022-06-08 17:52:01 +0800

    hide my nfts info on nft collection info page

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -18,94 +18,24 @@ export default function NFTDetails() {
   const { t } = useTranslation()
   const { query } = useRouter()
   const [nftInfo, setNFTInfo] = useState({})
-  const [holdingNFTs, setHoldingNFTs] = useState([])
-  const [withdrawnNFTs, setWithdrawnNFTs] = useState([])
-  const [pageOffset, setPageOffset] = useState(0)
-  const [pageCount, setPageCount] = useState(10)
-  const itemsPerPage = 10
   const { isLoggedIn, account } = useAccountFlashsigner()
+  const [loading, setLoading] = useState(false)
 
   const cotaId: string = (query.cotaId as string) || ""
   const { name, description, issued = 0, total = 0, image } = nftInfo as any
-  console.log("nftInfo: ", nftInfo)
 
   useEffect(() => {
     const fetchData = async () => {
       if (!cotaId) return
+      setLoading(true)
       const nft = await cotaService.aggregator.getDefineInfo({
         cotaId,
       })
       if (nft) setNFTInfo(nft)
+      setLoading(false)
     }
-    setInterval(fetchData, 5000)
+    fetchData()
   }, [cotaId])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!isLoggedIn) return
-      const cotaAddress = generateFlashsignerAddress(account.auth.pubkey)
-      const lockScript = serializeScript(addressToScript(cotaAddress))
-
-      try {
-        const holds = await cotaService.aggregator.getHoldCotaNft({
-          lockScript,
-          page: pageOffset,
-          pageSize: itemsPerPage,
-        })
-        console.log("aaa holds: ", holds)
-
-        const thisHoldings = holds.nfts.filter((nft) => nft.cotaId === cotaId)
-        setHoldingNFTs(thisHoldings as any)
-
-        const withdraws = await cotaService.aggregator.getWithdrawCotaNft({
-          lockScript,
-          page: pageOffset,
-          pageSize: itemsPerPage,
-        })
-        console.log("aaa withdraws: ", withdraws)
-        const thisWithdrawss = withdraws.nfts.filter((nft) => nft.cotaId === cotaId)
-        setWithdrawnNFTs(thisWithdrawss as any)
-
-        const newPageCount = thisHoldings.length + thisWithdrawss.length
-        console.log("newPageCount: ", newPageCount)
-        setPageCount(Math.ceil(newPageCount / itemsPerPage))
-      } catch (error) {
-        console.log("error: ", error)
-      }
-    }
-    setInterval(fetchData, 5000)
-  }, [pageOffset])
-
-  const handlePageChange = (event: any) => {
-    console.log(event)
-    // when its content is loaded in useEffect.
-    setPageOffset(event.selected)
-  }
-
-  const PaginatedItems = (
-    <>
-      <ReactPaginate
-        previousLabel="Previous"
-        nextLabel="Next"
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakLabel="..."
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageChange}
-        containerClassName="pagination"
-        activeClassName="active"
-        forcePage={pageOffset}
-      />
-    </>
-  )
 
   if (!cotaId) return <Loading />
 
@@ -136,22 +66,6 @@ export default function NFTDetails() {
             {t("nft.mint")}
           </Button>
         </Link>
-      </Stack>
-      <Stack direction={"column"} align={"center"} py={12}>
-        <Heading fontSize={"2xl"} fontWeight={500}>
-          {t("nft.myNFTs")}
-        </Heading>
-        <Flex marginTop={4} flexWrap="wrap" gap={4} p={0}>
-          {holdingNFTs.map((nft: NFTType) => (
-            <NFTInfo nft={nft} key={cotaId + nft.tokenIndex} />
-          ))}
-        </Flex>
-        <Flex marginTop={4} flexWrap="wrap" gap={4} p={0}>
-          {withdrawnNFTs.map((nft: NFTType) => (
-            <NFTInfo nft={nft} key={cotaId + nft.tokenIndex} />
-          ))}
-        </Flex>
-        {PaginatedItems}
       </Stack>
     </Stack>
   )
