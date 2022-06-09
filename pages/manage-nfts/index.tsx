630d0cbf5a302dc7602ac7f44d3f68f5fb43a679
HA <>
2022-06-09 12:26:03 +0800

    refactor manage nft page

--- pages/manage-nfts/index.tsx
+++ pages/manage-nfts/index.tsx
@@ -6,74 +6,69 @@ import { Flex, Heading, Box } from "@chakra-ui/react"
 import NFTInfo from "@components/nft/NFTInfo"
 import { NFTType } from "api/nft"
 import { useAccountFlashsigner } from "@lib/hooks/useAccount"
-import { generateFlashsignerAddress, ChainType, Config } from "@nervina-labs/flashsigner"
-import { cotaService } from "@lib/utils/ckb"
-import { addressToScript, serializeScript } from "@nervosnetwork/ckb-sdk-utils"
 import ReactPaginate from "react-paginate"
-import fetchers, { http } from "api/fetchers"
+import fetchers from "api/fetchers"
 import useSWR from "swr"
 
 export default function MyNFTsPage() {
   const { t } = useTranslation()
   const { account } = useAccountFlashsigner()
-  const cotaAddress = generateFlashsignerAddress(account.auth.pubkey)
-  const [serverApiNFTs, setServerApiNFTs] = useState([])
-  const lockScript = serializeScript(addressToScript(cotaAddress))
   const [pageOffset, setPageOffset] = useState(0)
   const [pageCount, setPageCount] = useState(10)
-  const itemsPerPage = 10
+  const itemsPerPage = 20
   const { data: apiNFTs, error } = useSWR(
     () => `${process.env.NEXT_PUBLIC_API_BASE}/mint-nft/account/${account.address}`,
     fetchers.http
   )
 
+  const nftAmount = apiNFTs?.result?.length || 0
+
   useEffect(() => {
     const fetchData = async () => {
-      const serverApiNFTs = apiNFTs?.result || []
-      setServerApiNFTs(serverApiNFTs as any)
-
-      const newPageCount = serverApiNFTs.length
+      const newPageCount = nftAmount
       setPageCount(Math.ceil(newPageCount / itemsPerPage))
     }
     fetchData()
-  }, [pageOffset])
+  }, [nftAmount, pageOffset])
 
   const handlePageChange = (event: any) => {
-    console.log(event)
-    // when its content is loaded in useEffect.
     setPageOffset(event.selected)
   }
 
-  const PaginatedItems = (
-    <>
-      <ReactPaginate
-        previousLabel="Previous"
-        nextLabel="Next"
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakLabel="..."
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageChange}
-        containerClassName="pagination"
-        activeClassName="active"
-        forcePage={pageOffset}
-      />
-    </>
-  )
+  let PaginatedItems
+
+  if (nftAmount > 0) {
+    PaginatedItems = (
+      <>
+        <ReactPaginate
+          previousLabel="Previous"
+          nextLabel="Next"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName="pagination"
+          activeClassName="active"
+          forcePage={pageOffset}
+        />
+      </>
+    )
+  }
 
   return (
     <Box>
       <Heading py={5}>{t("nft.manageNFTs")}</Heading>
       <Flex marginTop={4} flexWrap="wrap" gap={4} p={0}>
-        {serverApiNFTs?.map((nft: NFTType) => (
+        {apiNFTs?.result?.map((nft: NFTType) => (
           <NFTInfo nft={nft} key={nft.cotaId + nft.tokenIndex} />
         ))}
       </Flex>
